ecf85191264e0e9508643348eccc61ed7570ba31
2017011409 <machuan0228@sina.com>
2020-05-25 17:19:39 +0800

    initialize project

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace cv;
 
 int main(){
 	//读取本地的一张图片便显示出来
-	Mat img = imread("F:\\mm\\01.jpg");
+	Mat img = imread("DataSet/arborgreens/Image01.jpg");
+	imshow("Test", img);
+	cout << "size: " << img.size << endl;
 	//等待用户按键
 	waitKey(0);
 	return 0;
