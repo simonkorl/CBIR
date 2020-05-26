b79f30e684c67632eb9107954dd570eaf740bfd2
2017011409 <machuan0228@sina.com>
2020-05-26 21:12:27 +0800

    Finish test of making a  16bins histogram

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 #include <string>
 using namespace std;
 using namespace cv;
+typedef Point3_<uint8_t> Pixel;
 struct PicData {
 	string fileName;
 	int width = 0;
 	int height = 0;
 };
+const char DATASET_RELA_DIR[] = "DataSet/";
 int main(){
-	// Read images info in AllImage.txt
+	// Read images info from AllImage.txt
 	ifstream fin("AllImages.txt");
 	if (!fin.is_open()) {
 		cout << "Fail to open AllImages.txt" << endl;
@@ -25,10 +27,8 @@ int main(){
 		fin >> tmpData->fileName >> tmpData->width >> tmpData->height;
 		datas.push_back(tmpData);
 	}
-	for (auto data = datas.begin(); data != datas.end(); ++data) {
-		cout << (*data)->fileName << endl;
-	}
-	// Read query info in QueryImages.txt
+
+	// Read query info from QueryImages.txt
 	fin.close();
 	fin.open("QueryImages.txt");
 	vector<string> querys;
@@ -38,20 +38,53 @@ int main(){
 		fin >> queryName >> tmpNum >> tmpNum;
 		querys.push_back(queryName);
 	}
-	cout << "Querys:\n";
-	for (auto query = querys.begin(); query != querys.end(); ++query) {
-		cout << *query << endl;
+
+	// test to calculate Histogram of an BGR image
+	Mat testImg = imread(DATASET_RELA_DIR + datas[0]->fileName);
+	if (testImg.empty()) {
+		cout << "testImg has not been loaded" << endl;
+		return -1;
 	}
+	
+	int bin[16] = { 0 }, bin2[16] = { 0 };
+	int tmpCount = 0;
+	// This iteration seemed to be slower, but it guarantees 
+	// the total count of pixels
+	for (int i = 0; i < testImg.rows; ++i) {
+		for (int j = 0; j < testImg.cols; ++j) {
+			Pixel& pixel = testImg.at<Pixel>(i, j);
+			int b = 0, g = 0, r = 0;
+			b = pixel.x / 256.0 * 2;
+			g = pixel.y / 256.0 * 4;
+			r = pixel.z / 256.0 * 2;
+			bin[b * 8 + g * 2 + r]++;
+		}
+	}
+	// It is weird that forEach cannot iterate the whole image
+	// The total count in bin2 is not equal to that in bin
+	testImg.forEach<Pixel>(
+		[&bin2, &tmpCount](Pixel & pixel, const int* position) -> void{
+			int b = 0, g = 0, r = 0;
+			b = pixel.x / 256.0 * 2;
+			g = pixel.y / 256.0 * 4;
+			r = pixel.z / 256.0 * 2;
+			bin2[b * 8 + g * 2 + r]++;
+		}
+	);
+	int tmpSum = 0, tmpSum2 = 0;
+	for (int i = 0; i < 16; ++i) {
+		printf("bin[%d]: %d \t bin2[%d]: %d\n", i, bin[i],i, bin2[i]);
+		tmpSum += bin[i];
+		tmpSum2 += bin2[i];
+	}
+	printf("tmpSum: %d, tmpSum2: %d, width * height: %d", tmpSum , tmpSum2, (datas[0]->width * datas[0]->height));
+	// wait for user
+	// waitKey(0);
+
 	// release image info resource
 	while (datas.size() > 0) {
 		delete datas.back();
 		datas.pop_back();
 	}
-	////Load a local picture and display
-	//Mat img = imread("DataSet/arborgreens/Image01.jpg");
-	//imshow("Test", img);
-	//cout << "size: " << img.size << endl;
-	////wait for user
-	//waitKey(0);
 	return 0;
 }
