0c9c37088b65b2108ae617b449af2a13c759c344
2017011409 <machuan0228@sina.com>
2020-05-28 10:35:37 +0800

    Workflow  basically finished. Bugs occur in distance calculation

--- Query.h
+++ Query.h
@@ -3,5 +3,9 @@
 struct Query {
 	const PicInfo* picInfo = NULL;
 	double precision = 0;
-	const PicInfo* results[30] = { 0 };
+	std::vector<std::pair<const PicInfo*, double>> results;
+	void clear() {
+		precision = 0;
+		results.clear();
+	}
 };
